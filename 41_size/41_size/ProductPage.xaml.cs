bbbe5cd7c6b085ce253f8ddd075e27e301f61acd
Terriuawara Maguir <terriuawara@gmail.com>
2025-01-20 15:44:37 +0500

    18 лаба

--- 41_size/41_size/ProductPage.xaml.cs
+++ 41_size/41_size/ProductPage.xaml.cs
@@ -22,11 +22,74 @@ namespace _41_size
       InitializeComponent();
       var currentProducts = Kazychanov_41Entities.GetContext().Product.ToList();
       ProductListView.ItemsSource = currentProducts;
+
+      DiscountCB.SelectedIndex = 0;
+      UpdateProduct();
+    }
+
+    private void UpdateProduct()
+    {
+      var currentProducts = Kazychanov_41Entities.GetContext().Product.ToList();
+
+      //поиск по имени
+      currentProducts = currentProducts.Where(x => x.ProductName.ToLower().Contains(SearchTB.Text.ToLower())).ToList();
+      ProductListView.ItemsSource = currentProducts.ToList();
+
+
+
+      switch (DiscountCB.SelectedIndex)
+      {
+        case 1:
+          currentProducts = currentProducts.Where(x => Convert.ToInt32(x.ProductDiscountAmount) >= 0 && Convert.ToInt32(x.ProductDiscountAmount) < 10).ToList();
+          break;
+        case 2:
+          currentProducts = currentProducts.Where(x => Convert.ToInt32(x.ProductDiscountAmount) >= 10 && Convert.ToInt32(x.ProductDiscountAmount) < 15).ToList();
+          break;
+        case 3:
+          currentProducts = currentProducts.Where(x => Convert.ToInt32(x.ProductDiscountAmount) >= 15 && Convert.ToInt32(x.ProductDiscountAmount) < 100).ToList();
+          break;
+        default:
+          break;
+      }
+      ProductListView.ItemsSource = currentProducts;
+
+      //возрастание, убывание
+      if (UpRB.IsChecked.Value)
+      {
+        ProductListView.ItemsSource = currentProducts.OrderBy(x => x.ProductCost).ToList();
+      }
+      if (DownRB.IsChecked.Value)
+      {
+        ProductListView.ItemsSource = currentProducts.OrderByDescending(x => x.ProductCost).ToList();
+      }
+
+      //вывод количества продуктов в листвью из общего количества
+      NameCount.Text = "кол-во " + currentProducts.Count.ToString() + " из " + Kazychanov_41Entities.GetContext().Product.ToList().Count.ToString();
     }
 
     private void Button_Click(object sender, RoutedEventArgs e)
-    {
-      Manager.MainFrame.Navigate(new AddEditPage());
+      {
+        Manager.MainFrame.Navigate(new AddEditPage());
+      }
+
+      private void DownRB_Checked(object sender, RoutedEventArgs e)
+      {
+      UpdateProduct();
+    }
+
+      private void UpRB_Checked(object sender, RoutedEventArgs e)
+      {
+      UpdateProduct();
+    }
+
+      private void DiscountCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+      {
+      UpdateProduct();
+    }
+
+      private void SearchTB_TextChanged(object sender, TextChangedEventArgs e)
+      {
+      UpdateProduct();
+    }
     }
   }
-}
